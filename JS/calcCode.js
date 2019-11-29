26b591b8cbedc22e3e5a3183c914b02e97ce546a
juliaheller <julia.natsuko@gmail.com>
2019-11-29 13:29:10 +0100

    Create function to take the related string when a numer is clicked and show it on display1. Then tried to create a function which adds an operator to the display and replace only the operator if another operator is clicked. Bug: doesn't get the display1 value of first funktion.

--- JS/calcCode.js
+++ JS/calcCode.js
@@ -8,11 +8,17 @@ function takeNumber(number){
     document.getElementById("display1").innerHTML= display1;
 }
     
-function takeOperator(operator){
-    display1 = display1 + operator;
-    document.getElementById("display1").innerHTML= display1;
-
+function takeOperator(op){
+    if (operator !== "") {
+        operator = op;
+        display1 = display1.substring(0, display1.length -1);
+        debugger
+        display1 = display1 + operator;
+        document.getElementById("display1").innerHTML= display1;
+    }
+    else {
+        operator = op;
+        document.getElementById("display1").innerHTML= display1 + operator;
+    }
 }
    
-        
-    
