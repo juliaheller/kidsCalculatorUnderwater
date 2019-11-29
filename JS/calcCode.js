55756a7ffa51a94bdb8fffecf884e857f5042f50
juliaheller <julia.natsuko@gmail.com>
2019-11-29 11:42:25 +0100

    Create simple styles to make elements of the calculator visible and link file with index file.

---
+++ JS/calcCode.js
@@ -0,0 +1,18 @@
+var display1 = "";
+var display2 = ""; 
+var operator = "";
+var resultDisplay = "";
+
+function takeNumber(number){
+    display1 = display1 + number;
+    document.getElementById("display1").innerHTML= display1;
+}
+    
+function takeOperator(operator){
+    display1 = display1 + operator;
+    document.getElementById("display1").innerHTML= display1;
+
+}
+   
+        
+    
