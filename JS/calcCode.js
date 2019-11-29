42ab9fefa10059df82821f1b7ce638674584c58b
juliaheller <julia.natsuko@gmail.com>
2019-11-29 13:58:04 +0100

    Change tkaenumber function with adding an  if else condition so that the second display is used after an operator is clicked.

--- JS/calcCode.js
+++ JS/calcCode.js
@@ -4,15 +4,20 @@ var operator = "";
 var resultDisplay = "";
 
 function takeNumber(number){
-    display1 = display1 + number;
-    document.getElementById("display1").innerHTML= display1;
+    if (operator == ""){
+        display1 = display1 + number;
+        document.getElementById("display1").innerHTML= display1;
+    }
+    else {
+        display2 = display2 + number;
+        document.getElementById("display2").innerHTML= display2;
+    }
 }
     
 function takeOperator(op){
     if (operator !== "") {
         operator = op;
         display1 = display1.substring(0, display1.length -1);
-        debugger
         display1 = display1 + operator;
         document.getElementById("display1").innerHTML= display1;
     }
