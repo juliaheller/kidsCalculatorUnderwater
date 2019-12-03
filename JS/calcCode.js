153d84a56cb7d1982f33cedb5f13c9f415630a39
juliaheller <julia.natsuko@gmail.com>
2019-12-03 13:05:51 +0100

    hange calculate funtcion so that it is not easily hackable. Convert the display strings in numbers with parseInt. Before cut operator out of display1. Use the clicked operator with switch condition.

--- JS/calcCode.js
+++ JS/calcCode.js
@@ -28,8 +28,28 @@ function takeOperator(op){
     }
 }
    
-function calculate(equals){
-    resultDisplay = eval(display1 + display2);
+/*function calculate(equals){
+    resultDisplay = eval(display1 + display2); -->eval ist zwar n sich richtig, kann aber leicht gehackt werden
     document.getElementById("resultDisplay").innerHTML= resultDisplay;
+}*/
+
+function calculate(){
+    var number1 = display1.substring(0, display1.length -1); //Display1 wird um den Operator gekuerzt und in number 1 umgewandelt
+    number1 =  parseInt(number1); // Der String von number 1 wird in eine Zahl umgewandelt
+    var number2 = parseInt(display2);
+    switch (operator){
+        case "+":
+            document.getElementById("resultDisplay").innerHTML= number1 + number2;
+            break;
+        case "-":
+            document.getElementById("resultDisplay").innerHTML= number1 - number2;
+            break;
+        case "*":
+            document.getElementById("resultDisplay").innerHTML= number1 * number2;
+            break;
+        case "/":
+            document.getElementById("resultDisplay").innerHTML= number1 - number2;    
+    }
+ 
 }
 
